bf91f755d83fd0cdafce9160cd80c08f672b4f82
Capucine Trossat <ctrossat.dev@gmail.com>
2025-01-09 16:45:34 +0100

    first commit

---
+++ app/spellBlock.js
@@ -0,0 +1,38 @@
+'use client'
+
+import { useState } from "react"
+
+export function SpellBlock ({
+    name,
+    alias, 
+    type, 
+    level, 
+    castingTime, 
+    range, 
+    concentration, 
+    duration,
+    description
+}){
+
+    const [expend, setExpend] = useState(false)
+
+    return (
+        <div 
+            className='flex flex-col cursor-pointer px-4 py-2 rounded-md hover:bg-gray-200' 
+            onClick={()=>setExpend(!expend)}
+            >
+            <h2 className='text-lg'>
+              {name} {alias && '| '+ alias}
+            </h2>
+            <p>{type} de niveau {level}</p>
+            {expend &&  (
+                <div>
+                    <p>{castingTime} </p>
+                    <p>{range} </p>
+                    <p>Durée: {concentration && 'Concentration '} {duration}</p>
+                    <p className="mt-2">{description} </p>
+                </div>
+            )}
+          </div>
+    )
+}
